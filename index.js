895748269a2186134d13978dc25ff7584dbeace2
Sazid Ansari <sazidansari9634@gmail.com>
2024-12-07 07:09:02 +0530

    install nodejs and create new project and express package install and server runing and enpoint add

---
+++ index.js
@@ -0,0 +1,21 @@
+//import the express module
+const express = require('express');
+const helloRoute = require('./routes/hello');
+
+//Defind the port number the server will listen on
+const PORT = 3000;
+
+//create an instance of an express application
+//because it give us the starting point
+
+const app = express();
+
+//middlewere to register routes or to mount routes
+app.use(helloRoute);
+
+
+//start the server and listen on the specified port
+app.listen(PORT, "0.0.0.0", function(){
+    //LOG THE NUMBER
+    console.log(`server is running on port ${PORT}🚀`);
+})
